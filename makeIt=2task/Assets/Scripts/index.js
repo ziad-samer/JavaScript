4f367a3da54166ea74b11da8dd08c93324db7c76
ziadSamer <ziadsamer46@yahoo.com>
2023-06-01 14:59:37 +0300

    makeIt = 2

---
+++ makeIt=2task/Assets/Scripts/index.js
@@ -0,0 +1,6 @@
+function Addition() {
+    const firstNumber = document.getElementById("firstNumber").value;
+    const secondNumber = document.getElementById("secondNumber").value;
+    sum = Number(firstNumber) + Number(secondNumber)
+    alert('The result will be = ' + sum)
+}
